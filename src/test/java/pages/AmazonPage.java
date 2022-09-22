618ffddee1104af3709cde7ef588a24cac373ede
mu5tafacool <mustafakulf1@gmail.com>
2022-09-22 18:39:41 +0300

    22 09 22

--- src/test/java/pages/AmazonPage.java
+++ src/test/java/pages/AmazonPage.java
@@ -13,7 +13,6 @@ public class AmazonPage {
 
     @FindBy(id = "twotabsearchtextbox")
     public WebElement aramaKutusu;
-
     @FindBy(xpath = "//div[@class='a-section a-spacing-small a-spacing-top-small']")
     public WebElement aramaSonucElementi;
 }
